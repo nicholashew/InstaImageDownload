bfcbd4ab6c956a7ba68909347196dc5ed80cb5ef
Nicholas <nicholashew0511@gmail.com>
2018-09-30 00:19:41 +0800

    Add tagged image support and hacky fix to retrieve slider images

--- js/addToList.js
+++ js/addToList.js
@@ -1,28 +1,46 @@
 (function () {
-    const dialogImage = document.querySelector('div[role="dialog"] img[srcset]');
+    const addImageToStore = function (img) {
+        if (img) {
+            const srcsets = img.getAttribute("srcset").split(",");
+            const data = [];
 
-    if (dialogImage) {
-        const srcsets = dialogImage.getAttribute("srcset").split(",");
-        const data = [];
-
-        srcsets.forEach(function (el) {
-            const srcset = el.split(' ');
-            data.push({
-                src: srcset[0],
-                size: parseInt(srcset[1].slice(0, -1))
+            srcsets.forEach(function (el) {
+                const srcset = el.split(' ');
+                data.push({
+                    src: srcset[0],
+                    size: parseInt(srcset[1].slice(0, -1))
+                });
             });
-        });
 
-        let largest = data.reduce(function (prev, current) {
-            return (prev.size > current.size) ? prev : current
-        });
+            let largest = data.reduce(function (prev, current) {
+                return (prev.size > current.size) ? prev : current
+            });
 
-        if (largest) {
-            let url = largest.src;
-			let filename = url.substring(url.lastIndexOf('/') + 1);			
-			appStore.setItem(filename, url);
+            if (largest) {
+                let url = largest.src;
+                let filename = url.substring(url.lastIndexOf('/') + 1);
+                appStore.setItem(filename, url);
+            }
         }
-    } else {
-        alert('instagram popup dialog not found');
     }
+
+    // Posts dialog image
+    const dialogImages = document.querySelectorAll('div[role="dialog"] img[srcset]');
+
+    if (dialogImages.length > 0) {
+        dialogImages.forEach(function (el) {
+            addImageToStore(el);
+        });
+    } else {
+        // try to get from Tagged inner image
+        const taggedImages = document.querySelectorAll('img[srcset]');
+        
+        if (taggedImages.length > 0) {
+            taggedImages.forEach(function (el) {
+                addImageToStore(el);
+            });
+        } else {
+            alert('instagram posts dialog images or tagged details images not found');
+        }
+    }    
 })();
--- js/openLargeImage.js
+++ js/openLargeImage.js
@@ -1,28 +1,44 @@
 (function () {
-	const dialogImage = document.querySelector('div[role="dialog"] img[srcset]');
+    const openImageWindow = function (img) {
+        if (img) {
+            const srcsets = img.getAttribute("srcset").split(",");
+            const data = [];
 
-	if (dialogImage) {
-		const srcsets = dialogImage.getAttribute("srcset").split(",");
-		const data = [];
+            srcsets.forEach(function (el) {
+                const srcset = el.split(' ');
+                data.push({
+                    src: srcset[0],
+                    size: parseInt(srcset[1].slice(0, -1))
+                });
+            });
 
-		srcsets.forEach(function (el) {
-			const srcset = el.split(' ');
-			data.push({
-				src: srcset[0],
-				size: parseInt(srcset[1].slice(0, -1))
-			});
-		});
+            let largest = data.reduce(function (prev, current) {
+                return (prev.size > current.size) ? prev : current
+            });
 
-		let largest = data.reduce(function (prev, current) {
-			return (prev.size > current.size) ? prev : current
-		});
+            if (largest) {
+                window.open(largest.src, '_blank');
+            }
+        }
+    }
 
-		if (largest) {
-			let url = largest.src;
-			let filename = url.substring(url.lastIndexOf('/') + 1);
-			window.open(url, '_blank');
+    // Posts dialog image
+    const dialogImages = document.querySelectorAll('div[role="dialog"] img[srcset]');
+
+    if (dialogImages.length > 0) {
+        dialogImages.forEach(function (el) {
+            openImageWindow(el);
+        });
+    } else {
+        // try to get from Tagged inner image
+        const taggedImages = document.querySelectorAll('img[srcset]');
+		
+		if (taggedImages.length > 0) {
+            taggedImages.forEach(function (el) {
+                openImageWindow(el);
+            });
+        } else {
+            alert('instagram posts dialog images or tagged details images not found');
 		}
-	} else {
-		alert('instagram popup dialog not found');
-	}
+    }    
 })();
